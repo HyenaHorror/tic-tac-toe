15a56b373954ac1e1edf46652717d2fa906694a7
Rob Bird <robertbirdwv@gmail.com>
2018-06-14 09:12:26 -0400

    Can make moves on board.

--- methods.rb
+++ methods.rb
@@ -2,16 +2,28 @@ class Board
   # position_list = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 
   def initialize
-    puts "board initialized"
     @current_state = {
-      :A1 => "",  :A2 => "",  :A3 => "",
-      :B1 => "",  :B2 => "",  :B3 => "",
-      :C1 => "",  :C2 => "",  :C3 => ""
+      "A1" => "",  "A2" => "",  "A3" => "",
+      "B1" => "",  "B2" => "",  "B3" => "",
+      "C1" => "",  "C2" => "",  "C3" => ""
     }
     # puts @current_state
     # return @current_state
   end
+
   def return_board
     return @current_state
+  end
+
+  def return_position(pos)
+    return @current_state[pos]
+  end
+
+  def make_move(player, position)
+    if @current_state[position] == ""
+      @current_state[position] = player.to_s.upcase
+    else
+      return "Invalid move!"
+    end
   end
 end
--- test_methods.rb
+++ test_methods.rb
@@ -7,18 +7,42 @@ class TicTacToe_Test < Minitest::Test
   end
   def test_empty_board
     expected = {
-      :A1 => "",
-      :A2 => "",
-      :A3 => "",
-      :B1 => "",
-      :B2 => "",
-      :B3 => "",
-      :C1 => "",
-      :C2 => "",
-      :C3 => ""
+      "A1" => "",
+      "A2" => "",
+      "A3" => "",
+      "B1" => "",
+      "B2" => "",
+      "B3" => "",
+      "C1" => "",
+      "C2" => "",
+      "C3" => ""
     }
     actual = Board.new.return_board
     puts Board.new
+    assert_equal(expected, actual)
+  end
+  def test_make_move_a1_x
+    b = Board.new
+    b.make_move("X", "A1")
+    actual = b.return_board
+
+    expected = {
+      "A1" => "X", "A2" => "",  "A3" => "",
+      "B1" => "",  "B2" => "",  "B3" => "",
+      "C1" => "",  "C2" => "",  "C3" => ""
+    }
+    assert_equal(expected, actual)
+  end
+  def test_make_move_b3_o
+    b = Board.new
+    b.make_move("O", "B3")
+    actual = b.return_board
+
+    expected = {
+      "A1" => "", "A2" => "",  "A3" => "",
+      "B1" => "",  "B2" => "",  "B3" => "O",
+      "C1" => "",  "C2" => "",  "C3" => ""
+    }
     assert_equal(expected, actual)
   end
 end
