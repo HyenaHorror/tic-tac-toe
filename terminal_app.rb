e35074a717db98f7e166575bf011e80644a061f0
Rob Bird <robertbirdwv@gmail.com>
2018-06-14 10:18:38 -0400

    Updating board with player input.

--- terminal_app.rb
+++ terminal_app.rb
@@ -1,9 +1,9 @@
 require_relative 'methods.rb'
 
-board = Board.new
+$board = Board.new
 
-def display_board(board)
-  b = board.return_board
+def display_board
+  b = $board.return_board
   system "clear"
   puts """
   #{b["A1"]} | #{b["A2"]} | #{b["A3"]}
@@ -12,14 +12,24 @@ def display_board(board)
   ---------
   #{b["C1"]} | #{b["C2"]} | #{b["C3"]}
   """
+
   get_next_move_pvp
 end
 
+def get_next_move_pvp
+  print "Enter your move player #{$current_player}\n> "
+  move = gets.chomp.upcase
+  puts "move is: #{move}"
+  $board.make_move($current_player, move)
+  display_board
+end
+system "clear"
 puts "Welcome to Tic-Tac-Toe!\n\n(Player vs Player only)"
 puts "Ready to play?"
 ready = gets.chomp
 if ready.downcase == "y" || ready.downcase == "yes" || ready.downcase == "yeah"
-  #move on
+  $current_player = "X"
+  display_board
 else
   puts "Goodbye!"
 end
