3607150fda88a6dbc434e541803328f84ac3c63c
Rob Bird <robertbirdwv@gmail.com>
2018-06-21 16:39:08 -0400

    Select mode added. Added placeholders.

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 require 'erb'
 require_relative 'game.rb'
+enable :sessions
 
 get '/' do
+  erb :select_mode
+end
+
+post '/game_settings' do
+
+  redirect '/game_start'
+end
+
+get '/game' do
   erb :game
 end
