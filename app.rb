f4b16f4d510996556fc4ee99e564927834f0669a
hyenahorror <allyoursanity@gmail.com>
2018-08-09 16:40:22 -0400

    Switched to Game class

--- app.rb
+++ app.rb
@@ -8,10 +8,28 @@ get '/' do
 end
 
 post '/game_settings' do
+  session[:game] = Game.new
+  # session[:game] = Board.new
+  player1 = [params:player1]
+  player2 = [params:player2]
+  session[:current_player] = "x"
 
-  redirect '/game_start'
+  redirect 'game'
 end
 
 get '/game' do
-  erb :game
+  puts session[:game].return_board
+  erb :game, locals:{board:session[:game].return_board, size:3}
+end
+
+post '/make_move' do
+  session[:game].make_move(session[:current_player], params[:pos])
+  # session[:game].check_win
+  # session[:game].alt_player needs to be Game class
+  
+  redirect '/game'
+end
+get '/make_move2' do
+  session[:game].make_move(session[:current_player], params[:pos])
+  redirect '/game'
 end
